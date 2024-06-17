28e6577543da579550c4e032dd1be7d0f91ade88
Mani kandan <mm98@ford.com>
2024-06-17 14:20:38 +0530

    Axe latest changes added

--- run-axe.js
+++ run-axe.js
@@ -22,29 +22,31 @@
 //     process.exit(1);
 //   });
 // Example run-axe.js using axe-core
-const axe = require('axe-core');
-const fs = require('fs');
-const { JSDOM } = require('jsdom');
+const axe = require("axe-core");
+const fs = require("fs");
+const { JSDOM } = require("jsdom");
 
 const file = process.argv[2]; // Get the filename from command line arguments
 
-const html = fs.readFileSync(file, 'utf8');
+const html = fs.readFileSync(file, "utf8");
 
-const { window } = new JSDOM(html, {
-  // Options for jsdom (e.g., URL for relative resources) 
-  url: 'http://localhost/', // Important for accurate analysis 
-});
+const { window } = new JSDOM(html);
 
-axe.run(window.document.documentElement)
+// Now run axe within the jsdom context
+axe
+  .run(window.document.documentElement)
   .then((results) => {
-    if (results.violations.length > 0) {
-      console.error(JSON.stringify(results.violations, null, 2));
+    const criticalError = results.violations.filter(
+      (violation) => violation.impact === "critical"
+    );
+    if (criticalError.length > 0) {
+      console.error(JSON.stringify(criticalError, null, 2));
       process.exit(1); // Indicate error
     } else {
-      console.log('No accessibility violations found!');
+      console.log("No accessibility violations found!");
     }
   })
   .catch((err) => {
-    console.error('Error running axe:', err);
+    console.error("Error running axe:", err);
     process.exit(1);
   });
